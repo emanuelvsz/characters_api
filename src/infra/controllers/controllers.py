3587bf675fc0dfe408d813519189a95d2d427040
emanuelvsz <usermane13@gmail.com>
2023-07-25 03:29:33 -0300

    implementing db container and doing a query

--- src/infra/controllers/controllers.py
+++ src/infra/controllers/controllers.py
@@ -1,16 +1,48 @@
 from flask import jsonify, request
 from src.infra.mock.characters import characters
+import psycopg2
+
+# Função para realizar a conexão com o banco de dados
+def get_db_connection():
+    try:
+        connection = psycopg2.connect(
+            dbname="default_database",
+            user="postgres",
+            password="12345678",
+            host="localhost",  # Use o endereço IP do container do banco de dados se necessário
+            port="5432"        # A porta padrão do PostgreSQL é 5432
+        )
+        return connection
+    except Exception as e:
+        print("Erro ao conectar ao banco de dados:", e)
+        return None
 
 def getCharacters():
-    serialized_characters = []
-    for character in characters:
-        serialized_character = {
-            'id': character['id'],
-            'name': character['name'],
-            'from_where': character['from_where']
-        }
-        serialized_characters.append(serialized_character)
-    return jsonify(serialized_characters)
+    try:
+        connection = get_db_connection()
+        if not connection:
+            return jsonify({"error": "Erro ao conectar ao banco de dados"})
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, name, from_where FROM characters")
+        rows = cursor.fetchall()
+
+        serialized_characters = []
+        for row in rows:
+            serialized_character = {
+                'id': row[0],
+                'name': row[1],
+                'from_where': row[2]
+            }
+            serialized_characters.append(serialized_character)
+
+        cursor.close()
+        connection.close()
+
+        return jsonify(serialized_characters)
+
+    except Exception as e:
+        return jsonify({"error": "Erro ao buscar dados do banco de dados", "details": str(e)})
 
 
 def getCharacterByID(id):
